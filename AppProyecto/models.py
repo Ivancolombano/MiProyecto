0ba9e354f74d4270292273eeb2a50c9c8e0b77dd
Ivan Colombano <ivancolombano@hotmail.com>
2023-09-12 00:08:56 -0300

    campo agregado

--- AppProyecto/models.py
+++ AppProyecto/models.py
@@ -10,6 +10,7 @@ class Pasteleria(models.Model):
     creado = models.DateField(auto_now_add=True, null=True)
 
 
+
 class Cocina(models.Model):
 
     nombre = models.CharField(max_length=100)
@@ -18,10 +19,11 @@ class Cocina(models.Model):
     creado = models.DateField(auto_now_add=True, null=True)
 
 
+
 class Promociones(models.Model):
 
     nombre = models.CharField(max_length=200)
     descripcion = models.CharField(max_length=500)
     precio = models.DecimalField(max_digits=10, decimal_places=0, help_text='$')
     creado = models.DateField(auto_now_add=True, null=True)
-     
+
